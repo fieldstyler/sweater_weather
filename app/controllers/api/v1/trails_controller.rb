6361bd319f193064b72f298904207cd536917bda
fieldstyler <tdfields93@gmail.com>
2020-11-09 09:40:51 -0700

    Update Trails Controller and Create Trails Facade with initialize and attrs

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::TrailsController < ApplicationController
 
   def index
     location = params[:location]
-    render json: TrailsSerializer.new(TrailFacade.new(location))
+    render json: TrailsSerializer.new(TrailsFacade.new(location))
   end
 end
---
+++ app/facades/trails_facade.rb
@@ -0,0 +1,10 @@
+class TrailsFacade
+  attr_reader :location, :forecast, :trails
+  def initialize(location)
+    @location = location
+    @forecast = something
+    @trails = something
+  end
+
+  
+end
