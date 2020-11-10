e26406e635f13433b2e0dc55e31a91904a3db0a2
fieldstyler <tdfields93@gmail.com>
2020-11-10 11:51:15 -0700

    Update logo in background image

--- app/poros/background_image.rb
+++ app/poros/background_image.rb
@@ -5,7 +5,8 @@ class BackgroundImage
     @image_url = attr[:results][0][:urls][:raw]
     @credit = {
               source: 'https://images.unsplash.com/',
-              author: attr[:results][0][:user][:name]
+              author: attr[:results][0][:user][:name],
+              logo: 'https://unsplash-assets.imgix.net/marketing/press-symbol.svg?auto=format&fit=crop&q=60',
               }
   end
 end
